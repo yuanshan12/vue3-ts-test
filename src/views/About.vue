7f69b25f340cbca05fbacdce1effd17848eeea96
ryf <786817560@qq.com>
2023-10-13 13:54:02 +0800

    第一次提交

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>{{num}}</p>
+    <p>{{ num }}</p>
     <button @click="say('张三')">点击说话</button>
     <button @click="greeter(person)">接口</button>
   </div>
 </template>
 <script lang='ts'>
 // import {Options, Vue} from 'vue-class-component'
-import {ref, reactive, defineComponent} from 'vue'
+import { ref, reactive, defineComponent } from 'vue'
 export default {
-  setup () {
+  setup() {
     const num = ref(0)
 
-    function say (name: string) {
+    function say(name: string) {
       alert(name)
     }
 
     // 接口
 
     interface Person {
-      name: string,
+      name: string
       age: number
     }
 
-    function greeter (person: Person) {
+    function greeter(person: Person) {
       alert(`my name is ${person.name}, i'm ${person.age} years old`)
     }
 
@@ -34,7 +34,7 @@ export default {
       age: 18,
     })
 
-    return {num, say, greeter, person}
+    return { num, say, greeter, person }
   }
 }
 </script>>
